fe0c1cfe748dda37f6aac4be29454e27d188f60e
David Watkins <wm.david.watkins@gmail.com>
2015-08-27 14:28:28 -0500

    added more tests, actually computes std

--- stats.py
+++ stats.py
@@ -4,7 +4,14 @@ def mean(vals):
 	return float(total)/length
 
 def std(vals):
-	return 1.0
+	n = len(vals)
+	if n == 0:
+		return 0.0
+	mu = sum(vals)/n
+	var = 0.0
+	for val in vals:
+		var = var + (val-mu)**2
+	return (var/n)**0.5
 
 #print mean([3,4])
 
--- test_stats.py
+++ test_stats.py
@@ -17,3 +17,24 @@ def test_std1():
 	obs = std([0.0,2.0])
 	exp = 1.0
 	assert_equal(obs,exp)
+
+def test_std2():
+	obs = std([ ])
+	exp = 0.0
+	assert_equal(obs,exp)
+
+def test_std3():
+	obs = std([0.0,4.0])
+	exp = 2.0
+	assert_equal(obs,exp)
+
+def test_std4():
+	obs = std([1.0,3.0])
+	exp = 1.0
+	assert_equal(obs,exp)
+
+def test_std5():
+	obs = std([1.0,1.0,1.0])
+	exp = 0.0
+	assert_equal(obs,exp)
+
